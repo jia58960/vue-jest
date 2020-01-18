455eb229d74351fd6ff712827acc68504af5988d
ethanZhang <547221469@qq.com>
2020-01-18 15:00:50 +0800

    'test:添加vuex测试'

--- src/containers/TodoList/__tests__/integration/TodoList.test.js
+++ src/containers/TodoList/__tests__/integration/TodoList.test.js
@@ -2,10 +2,10 @@ import {
   mount
 } from '@vue/test-utils'
 import TodoList from '../../TodoList.vue'
-
+import store from '../../../../store/index'
 describe('TodoList组件集成测试', () => {
   test('1、 用户在输入框输入内容； 2、 用户按回车键； 3、 内容会显示在未完成列表中 ', () => {
-    const wrapper = mount(TodoList)
+    const wrapper = mount(TodoList, { store })
     const inputElem = wrapper.find('.todo-input')
     inputElem.setValue('Ethan')
     inputElem.trigger('change')
--- src/containers/TodoList/components/Header.vue
+++ src/containers/TodoList/components/Header.vue
@@ -4,7 +4,8 @@
       TODO：
       <input type="text"
       class="todo-input"
-      v-model="inputVal"
+      :value="inputVal"
+      @input=" e => changeInputVal(e.target.value)"
       @keyup.enter="addTodoItem"
       placeholder="请输入待办事项">
     </div>
@@ -12,12 +13,13 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'Header',
-  data () {
-    return {
-      inputVal: ''
-    }
+  computed: {
+    ...mapState({
+      inputVal: state => state.inputVal
+    })
   },
   methods: {
     addTodoItem () {
@@ -25,9 +27,12 @@ export default {
       const len = realVal.length
       if (realVal && len >= 4 && len <= 10 && !/\//.test(realVal)) {
         this.$emit('add', this.inputVal)
-        this.inputVal = ''
+        this.changeInputVal('')
       }
-    }
+    },
+    ...mapMutations({
+      changeInputVal: 'changeInputVal'
+    })
   }
 }
 </script>
