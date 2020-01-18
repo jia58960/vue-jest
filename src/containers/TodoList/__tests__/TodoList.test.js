ea5bcfb29be53e763c58078c27c119fba82757f7
ethanZhang <547221469@qq.com>
2020-01-18 11:35:46 +0800

    test:complete todolist component 100% coverage

--- src/containers/TodoList/__tests__/TodoList.test.js
+++ src/containers/TodoList/__tests__/TodoList.test.js
@@ -65,6 +65,23 @@ describe('TodoList 组件测试', () => {
     ])
   })
 
+  test('执行 resetUndoItem 方法，undoList 会复原', () => {
+    const wrapper = shallowMount(TodoList)
+    wrapper.setData({
+      undoList: [
+        { value: 1, status: 'div' },
+        { value: 2, status: 'input' },
+        { value: 3, status: 'div' }
+      ]
+    })
+    wrapper.vm.resetUndoItem()
+    expect(wrapper.vm.$data.undoList).toEqual([
+      { value: 1, status: 'div' },
+      { value: 2, status: 'div' },
+      { value: 3, status: 'div' }
+    ])
+  })
+
   test('执行 saveItem 方法，undoList 会更新元素最新值，且状态变更为div', () => {
     const wrapper = shallowMount(TodoList)
     wrapper.setData({
