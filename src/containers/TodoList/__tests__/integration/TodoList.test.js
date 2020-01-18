1ae62b4e08a30619071f60ee4f37dec5c521e618
ethanZhang <547221469@qq.com>
2020-01-18 14:30:35 +0800

    'test:BDD代码'

---
+++ src/containers/TodoList/__tests__/integration/TodoList.test.js
@@ -0,0 +1,17 @@
+import {
+  mount
+} from '@vue/test-utils'
+import TodoList from '../../TodoList.vue'
+
+describe('TodoList组件集成测试', () => {
+  test('1、 用户在输入框输入内容； 2、 用户按回车键； 3、 内容会显示在未完成列表中 ', () => {
+    const wrapper = mount(TodoList)
+    const inputElem = wrapper.find('.todo-input')
+    inputElem.setValue('Ethan')
+    inputElem.trigger('change')
+    inputElem.trigger('keyup.enter')
+    const todo = wrapper.findAll('.undo-list')
+    expect(todo.length).toBe(1)
+    expect(todo.at(0).text()).toContain('Ethan')
+  })
+})
